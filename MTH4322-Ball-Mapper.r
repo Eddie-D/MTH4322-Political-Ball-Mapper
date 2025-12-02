ccfc563f003f819f03704ff6ae94fec243e19668
eddiedoogan <eddiedoogan03@hotmail.co.uk>
2025-12-02 20:29:33 +0000

    Complete Ball Mapper for parties

--- MTH4322-Ball-Mapper.r
+++ MTH4322-Ball-Mapper.r
@@ -21,3 +21,23 @@ barplot(pts$Agree,
         horiz = TRUE,
         las = 1,
         xlim = c(0, max(csv$Agree) * 1.1)) # extend x-axis by 10%
+
+# Extract the parties
+parties <- subset(pts, select = c(Labour,
+                                  Conservative,
+                                  Liberal.Democrat,
+                                  Plaid.Cymru))
+parties <- normalize_to_min_0_max_1(parties)
+
+# Run the desired colouring
+color_by <- "Labour"
+color_by <- "Conservative"
+color_by <- "Liberal.Democrat"
+color_by <- "Plaid.Cymru"
+
+coloring <- data.frame(pts[[color_by]])
+
+graph <- BallMapper(parties, coloring, .20)
+ColorIgraphPlot(graph, store_in_file = paste("./", color_by, ".png"))
+print(graph)
+coloredDynamicNetwork(graph)
